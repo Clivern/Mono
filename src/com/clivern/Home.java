59db89464f0be9211b1ddb5bc441253eaceef7be
Clivern <hello@clivern.com>
2017-06-26 01:58:53 +0200

    class renamed

--- src/com/clivern/Home.java
+++ src/com/clivern/Home.java
@@ -4,7 +4,7 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-public class HelloWorld extends HttpServlet {
+public class Home extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
     throws IOException, ServletException
