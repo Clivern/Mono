ca31a7075121e60f284588353f10ca86f983e4ea
Clivern <hello@clivern.com>
2017-06-26 02:46:47 +0200

    add bash script to install tomcat & deploy servlet project

---
+++ bin/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ssh $1@$2 << EOF
+	cd /opt/tomcat/webapps
+	rm -rf mono
+	git clone https://github.com/Clivern/Mono.git mono
+	sudo chown -R tomcat:tomcat mono
+	sudo chmod -R 775 mono
+	javac mono/src/com/clivern/Home.java -classpath "/opt/tomcat/lib/servlet-api.jar" -d mono/WEB-INF/classes
+EOF
